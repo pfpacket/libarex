d0858836e89f7868be0fb17e9929f9d6b177aaeb
pf packet <afpacket@gmail.com>
2013-02-17 18:33:09 +0900

    layer2_switch: don't use const reference in std::map
    It is not recommended using const& of the second template parameter of std::map
    To solve this problem now use std::ref() and std::reference_wrapper.
    This may be the best way.

--- example/layer2_switch/interface_bridge.cpp
+++ example/layer2_switch/interface_bridge.cpp
@@ -24,13 +24,12 @@ public:
         boost::asio::io_service& ios,
         std::string const& nic1, std::string const& nic2) 
         :   counter_(0),
-            nic1_(nic1), nic2_(nic2),
             socket1_(ios, arex::packet_p_all::v4()),
             socket2_(ios, arex::packet_p_all::v4()),
             start1_(std::bind(&interface_bridge::recv_handler, this, _1, _2, NIC1)),
             start2_(std::bind(&interface_bridge::recv_handler, this, _1, _2, NIC2))
     {
-        init_mapper();
+        init_mapper(nic1, nic2);
         setup_bridge();
         start_receive(NIC1);
         start_receive(NIC2);
@@ -38,7 +37,7 @@ public:
 
 private:
 
-    void init_mapper();
+    void init_mapper(std::string const&, std::string const&);
     void setup_bridge();
     void start_receive(netif_t);
     void recv_handler(boost::system::error_code const&, int, netif_t);
@@ -53,7 +52,6 @@ private:
     }
     
     std::uint64_t counter_;
-    std::string const nic1_, nic2_;
     arex::packet_p_all::socket socket1_, socket2_;
     arex::packet_p_all::endpoint sender1_, sender2_;
     buffer_type buffer1_, buffer2_;
@@ -62,30 +60,30 @@ private:
     // Interface mappers
     std::map<netif_t, netif_t> ifdest_; // destination interface
     std::map<netif_t, std::string> ifname_; // interfaces' name
-    std::map<netif_t, arex::packet_p_all::socket&> ifsoc_;  // sockets
-    std::map<netif_t, arex::packet_p_all::endpoint&> ifep_; // endpoint
-    std::map<netif_t, buffer_type&> ifbuf_;
-    std::map<netif_t, handler_type&> ifhdl_;
+    std::map<netif_t, std::reference_wrapper<arex::packet_p_all::socket>> ifsoc_;  // sockets
+    std::map<netif_t, std::reference_wrapper<arex::packet_p_all::endpoint>> ifep_; // endpoint
+    std::map<netif_t, std::reference_wrapper<buffer_type>> ifbuf_;
+    std::map<netif_t, handler_type> ifhdl_;
 };
 
-void interface_bridge::init_mapper()
+void interface_bridge::init_mapper(std::string const& nic1, std::string const& nic2)
 {
-    ifdest_[NIC1] = NIC2;   ifdest_[NIC2] = NIC1;
-    ifname_[NIC1] = nic1_;  ifname_[NIC2] = nic2_;
-    ifsoc_.insert(make_ref_pair(NIC1, socket1_));
-    ifsoc_.insert(make_ref_pair(NIC2, socket2_));
-    ifep_.insert(make_ref_pair(NIC1, sender1_));
-    ifep_.insert(make_ref_pair(NIC2, sender2_));
-    ifbuf_.insert(make_ref_pair(NIC1, buffer1_));
-    ifbuf_.insert(make_ref_pair(NIC2, buffer2_));
-    ifhdl_.insert(make_ref_pair(NIC1, start1_));
-    ifhdl_.insert(make_ref_pair(NIC2, start2_));
+    ifdest_[NIC1]   = NIC2;   ifdest_[NIC2] = NIC1;
+    ifname_[NIC1]   = nic1;  ifname_[NIC2] = nic2;
+    ifsoc_.insert(std::make_pair(NIC1, std::ref(socket1_)));
+    ifsoc_.insert(std::make_pair(NIC2, std::ref(socket2_)));
+    ifep_.insert(std::make_pair(NIC1, std::ref(sender1_)));
+    ifep_.insert(std::make_pair(NIC2, std::ref(sender2_)));
+    ifbuf_.insert(std::make_pair(NIC1, std::ref(buffer1_)));
+    ifbuf_.insert(std::make_pair(NIC2, std::ref(buffer2_)));
+    ifhdl_.insert(std::make_pair(NIC1, std::ref(start1_)));
+    ifhdl_.insert(std::make_pair(NIC2, std::ref(start2_)));
 }
 
 void interface_bridge::setup_bridge()
 {
-    arex::packet_p_all::endpoint ep1("FF:FF:FF:FF:FF:FF", nic1_);
-    arex::packet_p_all::endpoint ep2("FF:FF:FF:FF:FF:FF", nic2_);
+    arex::packet_p_all::endpoint ep1("FF:FF:FF:FF:FF:FF", ifname_[NIC1]);
+    arex::packet_p_all::endpoint ep2("FF:FF:FF:FF:FF:FF", ifname_[NIC2]);
     socket1_.bind(ep1);
     socket2_.bind(ep2);
     make_promisc(socket1_, ep1.if_index());
@@ -96,7 +94,7 @@ void interface_bridge::start_receive(netif_t ifnum)
 {
     buffer_type& buf = ifbuf_.at(ifnum);
     buf.consume(buf.size());
-    ifsoc_.at(ifnum).async_receive_from(
+    ifsoc_.at(ifnum).get().async_receive_from(
         buf.prepare(65535),
         ifep_.at(ifnum),
         ifhdl_.at(ifnum)
@@ -115,20 +113,22 @@ void interface_bridge::recv_handler(
         
         // fillter out packets
         typedef arex::packet_p_all::endpoint::pkttype pkttype;
-        int pkt_type = ifep_.at(ifnum).packet_type();
+        int pkt_type = ifep_.at(ifnum).get().packet_type();
         if ((1522 < size || size < 32) || (pkt_type == pkttype::outgoing))
             break;
 
         arex::ethernet_header ethh;
         arex::streambuf_to_header(ethh, buffer);
-        std::cout << boost::format("No.%d\t%s\t%4d bytes  ") % ++counter_ % ifname_[ifnum] % size;
+        std::cout << boost::format("No.%-9d %-" +
+                std::to_string(std::max(ifname_[NIC1].length(), ifname_[NIC2].length()))
+                + "s  %4d bytes  ") % ++counter_ % ifname_[ifnum] % size;
         print_ether_header(ethh);
         
-        ifsoc_.at(ifdest_[ifnum]).send_to(
+        ifsoc_.at(ifdest_[ifnum]).get().send_to(
             buffer.data(),
             arex::packet_p_all::endpoint("0:0:0:0:0:0", ifname_[ifdest_[ifnum]])
         );
-    } while ( false ); }
+    } while (false); }
     start_receive(ifnum);
 }
 
@@ -143,8 +143,8 @@ void interface_bridge::make_promisc(arex::packet_p_all::socket& socket, int ifnu
 
 void interface_bridge::print_ether_header(arex::ethernet_header const& eth)
 {
-    std::cout << eth.source() << " --> " << eth.dest() 
-        << "  type=" << arex::ether_type_str(eth.eth_type()) << std::endl;
+    std::cout << boost::format("%17s --> %-17s type=%s\n")
+        % eth.source() % eth.dest() % arex::ether_type_str(eth.eth_type());
 }
 
 int main(int argc, char **argv)
